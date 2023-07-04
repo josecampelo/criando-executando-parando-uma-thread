5f4ae640ed01eae14d19989f12fd1397168c8033
José Campelo <josecampelojr@gmail.com>
2023-07-04 14:53:33 -0300

    Criando a thread e fazendo a lógica de executar e parar.

--- app/src/main/java/com/campelo/criandoexecutandoparandoumathread/MainActivity.kt
+++ app/src/main/java/com/campelo/criandoexecutandoparandoumathread/MainActivity.kt
@@ -10,8 +10,45 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private var pararThread = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        binding.btnExecutar.setOnClickListener {
+            minhaThread().start()
+        }
+
+        binding.btnParar.setOnClickListener {
+            pararThread = true
+            binding.btnExecutar.text = "Executar"
+            binding.btnExecutar.isEnabled = true
+        }
+    }
+
+    inner class minhaThread: Thread(){
+        override fun run() {
+            super.run()
+
+            repeat(10){i ->
+                if(pararThread){
+                    pararThread = false
+                    return
+                }
+                runOnUiThread{
+                    binding.btnExecutar.text = "Executando: $i T: ${currentThread()}"
+                    binding.btnExecutar.isEnabled = false
+
+                    if(i == 9){
+                        binding.btnExecutar.text = "Executar"
+                        binding.btnExecutar.isEnabled = true
+                    }
+
+                }
+
+                sleep(1000)
+            }
+        }
     }
 }
